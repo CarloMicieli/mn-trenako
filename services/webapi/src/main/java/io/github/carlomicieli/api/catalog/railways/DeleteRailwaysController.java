1d6f06ec25b7b6def0aabb0bb19b6bc2efaa15a6
CarloMicieli <carlo.micieli@gmail.com>
2024-01-01 10:07:10 +0100

    feat(catalog): initial commit for the railways deletion endpoint

---
+++ services/webapi/src/main/java/io/github/carlomicieli/api/catalog/railways/DeleteRailwaysController.java
@@ -0,0 +1,38 @@
+/*
+ *   Copyright (c) 2024 (C) Carlo Micieli
+ *
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ */
+package io.github.carlomicieli.api.catalog.railways;
+
+import io.github.carlomicieli.api.catalog.CatalogApis;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Produces;
+
+@Controller(CatalogApis.API_RAILWAYS)
+public class DeleteRailwaysController {
+    @Delete("/{railwayId}")
+    @Produces(MediaType.APPLICATION_JSON_PROBLEM)
+    public HttpResponse<Void> deleteBrand(@PathVariable("railwayId") String railwayId) {
+        return HttpResponse.noContent();
+    }
+}
